d330f6720584a940fda91808f39af49c2c353c46
Talha Yazar <talhayazarg@gmail.com>
2022-09-19 23:59:31 +0300

    Created our printCommandEvents function to follow our flow and made it concurrent

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
+
 import (
+	"fmt"
 	"github.com/shomali11/slacker"
 	"os"
 )
+
 func main() {
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-")
 	os.Setenv("SLACK_APP_TOKEN", "xapp-")
+
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+}
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
 }
