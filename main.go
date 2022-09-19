f7b0537de6cd75f0e0389f86c0b459bca32331eb
Talha Yazar <talhayazarg@gmail.com>
2022-09-20 00:18:11 +0300

    Created our first bot command as an age calculator

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/shomali11/slacker"
+	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -14,7 +18,34 @@ func main() {
 
 	go printCommandEvents(bot.CommandEvents())
 
+	bot.Command("I born in <year>", &slacker.CommandDefinition{
+		Description: "Age Calculator",
+		Examples:    []string{"I born in 1990"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			birthYear, err := strconv.Atoi(year)
+
+			if err != nil || birthYear > time.Now().Year() {
+				response.Reply("Please enter a valid year")
+				return
+			}
+
+			age := time.Now().Year() - birthYear
+
+			r := fmt.Sprintf("You are %d years old", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
 		fmt.Println("Command Events")
